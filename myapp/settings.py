7fa70deecbdcec5ce72ef6b6ab62ebe3e4a75af7
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-09-19 23:09:19 +0000

    static

--- myapp/settings.py
+++ myapp/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,12 +82,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 DEFAULT_DATABASE_DSN = os.environ.get('DEFAULT_DATABASE_DSN')
 DATABASES = {'default': dj_database_url.parse(DEFAULT_DATABASE_DSN)}
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 
 # Password validation
@@ -126,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
