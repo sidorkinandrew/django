8f4a1d5937f5340a7e88ad5b4ae879e06b81a88f
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-09-20 11:54:59 +0000

    modified docker

--- docker.sh
+++ docker.sh
@@ -7,3 +7,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo docker run hello-world
 sudo apt-get install docker-compose -y
 # https://docs.docker.com/compose/django/
+# git commit -a -m "modified docker"
