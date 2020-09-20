1fa6de8c4995b99217ac4347c6c44d8496c96d9c
sidorkinandrew <47028236+sidorkinandrew@users.noreply.github.com>
2020-09-20 14:24:17 +0000

    docker up

--- djangoexample/settings.py
+++ djangoexample/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'z_nsd0js%@a2*^#-a$q8q0-5vcw=6eslnnc3kq6kfyn^33h6f@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 'pythondev.ml']
 
 
 # Application definition
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'djangoexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
